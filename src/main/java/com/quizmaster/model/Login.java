b7861a2480d3d725e9495a0e990a02be20e27f77
biribiriA <barihamim71@gmail.com>
2018-05-16 13:08:30 +0600

    Design login and sign up model class

---
+++ src/main/java/com/quizmaster/model/Login.java
@@ -0,0 +1,25 @@
+package com.quizmaster.model;
+
+public class Login {
+    private String userId;
+    private String userPassword;
+
+    public Login() {
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
---
+++ src/main/java/com/quizmaster/model/Topic.java
@@ -0,0 +1,16 @@
+package com.quizmaster.model;
+
+public class Topic {
+    private String topicSelection;
+
+    public Topic() {
+    }
+
+    public String getTopicSelection() {
+        return topicSelection;
+    }
+
+    public void setTopicSelection(String topicSelection) {
+        this.topicSelection = topicSelection;
+    }
+}
